3692e4c16b273878052a4859ea71528337389ae9
Femi Oladele <profemzy@gmail.com>
2021-05-28 02:15:28 +0100

    Update Book Controller

--- Cool/Controllers/BookAPIController.cs
+++ Cool/Controllers/BookAPIController.cs
@@ -10,8 +10,8 @@ namespace Cool.Controllers
     [Route("api/books")]
     public class BookApiController : Controller
     {
-        private ResponseDto _response;
-        private IBookRepository _bookRepository;
+        private readonly ResponseDto _response;
+        private readonly IBookRepository _bookRepository;
 
         public BookApiController(IBookRepository bookRepository)
         {
@@ -24,7 +24,8 @@ namespace Cool.Controllers
         {
             try
             {
-                IEnumerable<BookDto> bookDtos = await _bookRepository.GetBooks();
+                var bookDtos = await _bookRepository.GetBooks();
+                _response.IsSuccess = true;
                 _response.Result = bookDtos;
             }
             catch (Exception ex)
@@ -33,6 +34,81 @@ namespace Cool.Controllers
                 _response.ErrorMessages = new List<string>() {ex.ToString()};
             }
 
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                var bookDto = await _bookRepository.GetBook(id);
+                _response.IsSuccess = true;
+                _response.Result = bookDto;
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<object> Post([FromBody] BookDto bookDto)
+        {
+            try
+            {
+                var model = await _bookRepository.CreateUpdateBook(bookDto);
+                _response.IsSuccess = true;
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() {ex.ToString()};
+            }
+
+            return _response;
+        }
+        
+        [HttpPut]
+        public async Task<object> Put([FromBody] BookDto bookDto)
+        {
+            try
+            {
+                var model = await _bookRepository.CreateUpdateBook(bookDto);
+                _response.IsSuccess = true;
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+        
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                var isSuccess = await _bookRepository.DeleteBook(id);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                    = new List<string>() { ex.ToString() };
+            }
             return _response;
         }
     }
