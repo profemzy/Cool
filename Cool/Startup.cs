efff920b2e68baacfab0127433bce35b44179f3b
Femi Oladele <profemzy@gmail.com>
2021-07-01 08:48:37 +0100

    Switch DB Server

--- Cool/Startup.cs
+++ Cool/Startup.cs
@@ -24,7 +24,7 @@ namespace Cool
         public void ConfigureServices(IServiceCollection services)
         {
 
-            const string connection = @"Server=ms-sql-server,1433;Database=bookDb;User=sa;Password=Password2020;";
+            const string connection = @"Server=mssql-service,1433;Database=bookDb;User=sa;Password=Password2020;";
             
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connection));
